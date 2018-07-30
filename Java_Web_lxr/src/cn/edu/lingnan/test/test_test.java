50bd55a5fec7f9c192b474f511cc1d33c1eb12e4
'LXR1998' <'1163870414@qq.com'>
2018-07-30 10:37:22 +0800

    emmm，又重新弄了一遍

--- Java_Web_lxr/src/cn/edu/lingnan/test/test_test.java
+++ Java_Web_lxr/src/cn/edu/lingnan/test/test_test.java
@@ -3,7 +3,7 @@ package cn.edu.lingnan.test;
 public class test_test {
 
 	public static void main(String[] args) {
-		System.out.println("啊啊啊啊啊~~~~~~~~随便啦");
+		System.out.println("啊啊啊啊啊~~~~~~~~没事的，随便啦");
 
 	}
 
